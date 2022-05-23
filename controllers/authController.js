c3abaf3b7d3dc97f695879982ed37acb6194ee48
Niket Bahety <niketbahety@gmail.com>
2022-05-23 17:31:18 +0530

    Bug fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,7 @@ const createSendToken = (user, statusCode, req, res) => {
             Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-        secure: req.secure,
+        secure: true,
         sameSite: 'none',
     });
 
